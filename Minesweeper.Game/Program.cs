cc97da370c3d3cd122de991e1f4d82d609709275
Alexandros Kritikos <akritikos@outlook.com>
2019-12-18 14:14:11 +0200

    Simplifies area drawing loops

--- Minesweeper.Game/Program.cs
+++ Minesweeper.Game/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -18,8 +18,13 @@ namespace Minesweeper
         private const int borderHeight = maxHeight - borderSize;
         private const int cursorStartingPosX = 5;
         private const int cursorStartingPosY = 5;
-        private const char playableAreaHorizontalSymbol = '-';
-        private const char playableAreaVerticalSymbol = '|';
+        private const char playableAreaHorizontalSymbol = '─';
+        private const char playableIntersectionSymbol = '┼';
+        private const char playableLeftCross = '├';
+        private const char playableRightCross = '┤';
+        private const char playableTopCross = '┬';
+        private const char playableBottomCross = '┴';
+		private const char playableAreaVerticalSymbol = '│';
         private const char cursorSymbol = 'O';
         private const char bombSymbol = 'X';
 
@@ -40,292 +45,25 @@ namespace Minesweeper
 
         public static void DrawPlayableArea()
         {
-            for (int j = borderSize + 1; j < borderWidth; j++)
-            {
-                Console.SetCursorPosition(j, borderSize * 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 2 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 3);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 3 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 4);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 4 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 5);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 5 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 6);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 6 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 7);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 7 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 8);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 8 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-
-                Console.SetCursorPosition(j, borderSize * 9 - 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaHorizontalSymbol);
-            }
-
-
-            for (int i = borderSize + 1; i < borderHeight; i++)
-            {
-                Console.SetCursorPosition(borderSize * 2, i );
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 2 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 3, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 3 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 4, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 4 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 5, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 5 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 6, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 6 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 7, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 7 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 8, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 8 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 9, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 9 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 10, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 10 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 11, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 11 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 12, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 12 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 13, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 13 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 14, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 14 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 15, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 15 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 16, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 16 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 17, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 17 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 18, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 18 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 19, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 19 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 20, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 20 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 21, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 21 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 22, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 22 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 23, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 23 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 24 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 24, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 25 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 25, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 26 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 26, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 27 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 27, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 28 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 28, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-
-                Console.SetCursorPosition(borderSize * 29 - 2, i);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(playableAreaVerticalSymbol);
-            }
+	        for (var j = borderSize+1; j < borderHeight; j+=2)
+	        {
+		        for (var i = borderSize+1; i < borderWidth; i++)
+		        {
+			        Console.SetCursorPosition(i, j);
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.Write(playableAreaHorizontalSymbol);
+				}
+	        }
+
+	        for (var i = borderSize + 1; i < borderWidth; i += 2)
+	        {
+		        for (var j = borderSize + 1; j < borderHeight; j++)
+		        {
+			        Console.SetCursorPosition(i, j);
+			        Console.ForegroundColor = ConsoleColor.Red;
+			        Console.Write(playableAreaVerticalSymbol);
+		        }
+	        }
         }
 
         public static void CursorMovement()
